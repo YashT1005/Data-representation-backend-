ba679190d0974ad5a6992b314fc81dd0e846bb3e
YashT1005 <yashthakur10052@gmail.com>
2024-05-08 02:52:32 +0530

    first commit

---
+++ database/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { config } from "dotenv";
+
+// database connection
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
